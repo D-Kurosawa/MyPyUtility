4a73a59a0160c51a1c66bbf45ce7ba31a9c935a4
Daisuke Kurosawa <dai-kuro@sings.jp>
2018-11-08 14:56:15 +0900

    [add] ファイルを削除する機能を追加

---
+++ src/script_file_delete.py
@@ -0,0 +1,57 @@
+"""ルートディレクトリ以下のファイルを全て削除するスクリプト"""
+import os
+
+
+def main():
+    print("=" * 80)
+
+    path = os.getcwd().replace(os.sep, '/')
+    print(f"右記ディレクト支配下にあるファイルを一括削除します : {path}")
+
+    if input_selector():
+        file_delete(path)
+        print('処理完了')
+    else:
+        print('処理を中止しました')
+
+    print("=" * 80)
+
+
+def input_selector():
+    """標準入力でYes or Noの入力を求めboolを返す
+
+    :rtype: bool
+    """
+    dic = {'y': True, 'n': False}
+
+    while True:
+        try:
+            result = dic[input('[Y]es/[N]o >> ').lower()[0]]
+            break
+        except KeyError:
+            pass
+        print('Error! Input again.')
+
+    return result
+
+
+def file_delete(path):
+    """ファイルを削除する
+
+    :type path: str
+    """
+    script = os.path.abspath(__file__).replace(os.sep, '/')
+
+    print()
+    for root, _, files in os.walk(path):
+        for f in files:
+            file = os.path.join(root, f).replace(os.sep, '/')
+            if file != script:
+                print(f">> Delete : {file}")
+                os.remove(os.path.join(root, file))
+
+    print()
+
+
+if __name__ == '__main__':
+    main()
